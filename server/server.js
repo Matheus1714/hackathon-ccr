a907c49c8ad148baeb2ab8e71101926084d58b63
Matheus Mota <matheussilvamartins1714@gmail.com>
2020-06-13 11:09:54 -0300

    server error response

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const HereAPI = require('./hereapi')
+const cors = require('cors')
 
 let KEY = process.env.HERE_API_KEY;
 
 const app = express();
 const here = new HereAPI(KEY);
 
+app.use(cors())
+
 app.get('/searchbyaddress', async (req,res) => {
     let address = req.query.address;
     let positions = await here.searchGasStationsByAddress(address);
 
-    return req.send(JSON.stringify(positions));
+    return res.send(JSON.stringify(positions));
 });
 
 app.listen(9090, () => {
